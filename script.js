a23b2ca6f37cfe36dcfef072db9d81eba7f96360
Larissa Martins <larissa.martins@inboundcycle.com>
2023-09-14 21:20:42 -0300

    Feat: add nav sticky funcionality w/intersection observer API

--- script.js
+++ script.js
@@ -108,3 +108,25 @@ const handleHover = function (e) {
 // reducing the need to pass the opacity argument explicitly every time
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+
+// Sticky navegation w/ Intersection observer API
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
